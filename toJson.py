726e3f2fcdfc1cb684be019546daa094c9b13f0c
Martijn-Chemrade <142786732+Martijn-Chemrade@users.noreply.github.com>
2025-04-25 12:19:02 +0200

    defined regex for each of the parts. Not using tokens anymore

--- toJson.py
+++ toJson.py
@@ -1,21 +1,39 @@
 
 from typing import Tuple
+import re
 
-TOKEN_MAP = {
-    "Id:": "Product_id",
-    "ASIN:": "Product_ASIN",
-    "title:": "Product_title",
-    "group:": "Product_group",
-    "salesrank:": "Product_salesrank",
-    "similar:": "Similar_products",
-    "cutomer:": "Customer",
-    "categories:": "Product_categories",
-    "reviews:": "Product_reviews",
-    "rating:": "Review_rating",
-    "votes:" : "Review_votes",
-    "helpful:": "Review_votes_helpful",
-    "discontinued product": "Product_discontinued",
-}
+RE_ID = r"Id:\s*(.*)(?=\n)"
+RE_ASIN = r"ASIN:\s*(.*)(?=\n)"
+RE_TITLE = r"title: (.*)(?=\n)"
+RE_GROUP = r"group: (.*)(?=\n)"
+RE_SALESRANK = r"salesrank: (.*)(?=\n)"
+RE_SIMILAR = r"similar: (.*)(?=\n)"
+RE_TOTAL = r"total: (\d*)"
+RE_DOWNLOADED = r"downloaded: (\d*)"
+RE_AVG_RATING = r"avg rating: (\d*)"
+RE_DISCONTINUED = r"(discontinued product)"
+RE_DATE = r"([^\s]*)\s*cutomer:"
+RE_CUSTOMER = r"cutomer:\s*([^\s]*)"
+RE_RATING = r"rating:\s*(\d*)"
+RE_VOTES = r"votes:\s*(\d*)"
+RE_HELPFUL = r"helpful:\s*(\d*)"
+
+PRODUCT_ID = "Product_id"
+PRODUCT_ASIN = "Product_ASIN"
+PRODUCT_TITLE = "Product_title"
+PRODUCT_GROUP = "Product_group"
+PRODUCT_SALESRANK = "Product_salesrank"
+PRODUCT_SIMILAR = "Similar_products"
+REVIEW_CUSTOMER = "Review_Customer"
+PRODUCT_CATEGORIES = "Product_categories"
+PRODUCT_REVIEWS = "Product_reviews"
+PRODUCT_TOTAL_REVIEWS = "Review_total_count"
+PRODUCT_DOWNLOADED_REVIEWS = "Review_downloaded_count"
+PRODUCT_AVG_RATING = "Product_avg_rating"
+REVIEW_RATING = "Review_rating"
+REVIEW_VOTES = "Review_votes"
+REVIEW_HELPFUL = "Review_votes_helpful"
+PRODUCT_DISCONTINUED = "Product_discontinued"
 
 def read_product_from_index(text: str, start_index: int) -> Tuple[dict, int]: # return dictionary of product and index of last line of product
     product = {}
